0618d78ec13abd9c9302ea049f05f0c36c6e0e16
na0 <skdud907@naver.com>
2023-08-30 00:30:05 +0900

    UI 상태 저장

--- app/src/main/java/com/example/counter/MainActivity.kt
+++ app/src/main/java/com/example/counter/MainActivity.kt
@@ -1,26 +1,51 @@
 package com.example.counter
 
+import android.app.Activity
+import android.app.Application
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import com.example.counter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
-    var count = 0
+    private val viewModel by viewModels<MainViewModel>()
 
+    lateinit var binding: ActivityMainBinding
+
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        binding.counterText.text = "${viewModel.count}"
+
         binding.addButton.setOnClickListener {
-            count++
-            binding.counterText.text = "$count"
+            viewModel.count++
+            binding.counterText.text = "${viewModel.count}"
         }
 
         binding.minusButton.setOnClickListener {
-            count--
-            binding.counterText.text = "$count"
+            viewModel.count--
+            binding.counterText.text = "${viewModel.count}"
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("count", viewModel.count)
+    }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        viewModel.count = savedInstanceState.getInt("count")
+        Log.d(TAG, "onRestoreInstanceState: ${viewModel.count}")
+    }
+
+    companion object {
+        val TAG = MainActivity::class.java.simpleName
     }
 }
---
+++ app/src/main/java/com/example/counter/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.counter
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    var count = 0
+}
